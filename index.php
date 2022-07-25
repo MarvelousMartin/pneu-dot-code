2a8bd4b846e5e3f7eda25fe64dc8f81beab150da
Martin <oakk.martin@gmail.com>
2022-07-25 21:04:43 +0200

    Update

--- index.php
+++ index.php
@@ -34,39 +34,32 @@ if (strlen($text) == 12) {
     $third = $arr[1];
     $fourth = substr($arr[2], 0, 2);
     $fifth = substr($arr[2], 2, 2);
+} else {
+    echo "Chyba - DOT kód musí mít 12 číslic";
 }
-else if (strlen($text) == 13) {
-    $arr = str_split($text, 4);
-    $first = substr($arr[0], 0, 2);
-    $second = substr($arr[0], 2, 2);
-    $third = $arr[1];
-    $fourth = substr($arr[2], 0, 2);
-    $fifth = substr($arr[2], 2, 2);
-}
-
 ?>
 <div class="flex justify-center text-center m-5 w-full mx-auto pt-12 w-3/4 text-xl font-bold">
-    <div class="text-black px-2 py-3 mx-1 text-lg">
+    <div class="md:w-1/12 text-white bg-black rounded-lg px-2 py-3 mx-1 ring-1 ring-slate-900/5 shadow-xl">
         <a>DOT</a>
     </div>
-    <div class="w-1/12 text-white bg-green-600 rounded-lg px-2 py-3 mx-1 ring-1 ring-slate-900/5 shadow-xl">
+    <div class="md:w-1/12 text-white bg-green-700 rounded-lg px-2 py-3 mx-1 ring-1 ring-slate-900/5 shadow-xl">
         <a><?php echo $first ?></a>
     </div>
-    <div class="w-1/12 text-white bg-red-600 rounded-lg px-2 py-3 mx-1 ring-1 ring-slate-900/5 shadow-xl">
+    <div class="md:w-1/12 text-white bg-red-700 rounded-lg px-2 py-3 mx-1 ring-1 ring-slate-900/5 shadow-xl">
         <a><?php echo $second ?></a>
     </div>
-    <div class="w-1/6 text-white bg-yellow-600 rounded-lg px-2 py-3 mx-1 ring-1 ring-slate-900/5 shadow-xl">
+    <div class="md:w-1/10 lg:w-1/6 text-white bg-yellow-600 rounded-lg px-2 py-3 mx-1 ring-1 ring-slate-900/5 shadow-xl">
         <a><?php echo $third ?></a>
     </div>
-    <div class="w-1/12 text-white bg-blue-600 rounded-lg px-2 py-3 mx-1 ring-1 ring-slate-900/5 shadow-xl">
+    <div class="md:w-1/12 text-white bg-blue-600 rounded-lg px-2 py-3 mx-1 ring-1 ring-slate-900/5 shadow-xl">
         <a><?php echo $fourth ?></a>
     </div>
-    <div class="w-1/12 text-white bg-purple-700 rounded-lg px-2 py-3 mx-1 ring-1 ring-slate-900/5 shadow-xl">
+    <div class="md:w-1/12 text-white bg-purple-700 rounded-lg px-2 py-3 mx-1 ring-1 ring-slate-900/5 shadow-xl">
         <a><?php echo $fifth ?></a>
     </div>
 </div>
-<div class="flex justify-between m-5 w-4/5 mx-auto pt-12">
-    <div class="w-1/2 bg-blue-700 rounded-lg px-6 py-8 m-3 ring-1 ring-slate-900/5 shadow-xl">
+<div class="flex-col md:flex justify-center m-5 w-4/5 mx-auto pt-12">
+    <!--<div class="w-1/2 bg-blue-700 rounded-lg px-6 py-8 m-3 ring-1 ring-slate-900/5 shadow-xl">
         <div>
             <span class="inline-flex items-center justify-center p-2 bg-slate-300 rounded-md shadow-lg">
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-patch-question" viewBox="0 0 16 16">
@@ -82,8 +75,8 @@ else if (strlen($text) == 13) {
             vyčíst určité informace, které se pojí s původem pneumatiky, a to včetně orientačního data její výroby,
             jež najdete v posledním čtyřčíslí DOT kódu
         </p>
-    </div>
-    <div class="w-1/4 bg-green-600 rounded-lg px-6 py-8 m-3 ring-1 ring-slate-900/5 shadow-xl" id="dropdown">
+    </div>-->
+    <div class="mx-auto w-10/12 md:w-1/2 lg:w-1/4 bg-green-700 rounded-lg px-6 py-8 m-3 ring-1 ring-slate-900/5 shadow-xl" id="dropdown">
         <div>
             <span class="inline-flex items-center justify-center p-2 bg-slate-300 rounded-md shadow-lg">
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-building" viewBox="0 0 16 16">
@@ -93,14 +86,35 @@ else if (strlen($text) == 13) {
             </span>
         </div>
         <h3 class="text-black mt-5 text-base font-medium tracking-tight font-bold text-2xl">Kód výrobce</h3>
+        <br />
         <a class="text-white mt-2 text-3xl font-bold">
             <?php echo $first ?>
         </a>
-        <a class="ml-5 text-white mt-2 text-3xl font-bold">
+        <br />
+        <a class="text-white mt-2 text-lg font-bold">
             Testovací firma
         </a>
     </div>
-    <div class="w-1/4 bg-green-600 rounded-lg px-6 py-8 m-3 ring-1 ring-slate-900/5 shadow-xl" id="dropdown">
+    <div class="mx-auto w-10/12 md:w-1/2 lg:w-1/4 bg-red-700 rounded-lg px-6 py-8 m-3 ring-1 ring-slate-900/5 shadow-xl" id="dropdown">
+        <div>
+            <span class="inline-flex items-center justify-center p-2 bg-slate-300 rounded-md shadow-lg">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-slash-circle" viewBox="0 0 16 16">
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                  <path d="M11.354 4.646a.5.5 0 0 0-.708 0l-6 6a.5.5 0 0 0 .708.708l6-6a.5.5 0 0 0 0-.708z"/>
+                </svg>
+            </span>
+        </div>
+        <h3 class="text-black mt-5 text-base font-medium tracking-tight font-bold text-2xl">Rozměr pneu</h3>
+        <br />
+        <a class="text-white mt-2 text-3xl font-bold">
+            <?php echo $second ?>
+        </a>
+        <br />
+        <a class="text-white mt-2 text-lg font-bold">
+            Testovací rozměr
+        </a>
+    </div>
+    <div class="mx-auto w-10/12 md:w-1/2 lg:w-1/4 bg-yellow-600 rounded-lg px-6 py-8 m-3 ring-1 ring-slate-900/5 shadow-xl" id="dropdown">
         <div>
             <span class="inline-flex items-center justify-center p-2 bg-slate-300 rounded-md shadow-lg">
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-building" viewBox="0 0 16 16">
@@ -109,12 +123,40 @@ else if (strlen($text) == 13) {
                 </svg>
             </span>
         </div>
-        <h3 class="text-black mt-5 text-base font-medium tracking-tight font-bold text-2xl">Rozměr pneumatiky</h3>
+        <h3 class="text-black mt-5 text-base font-medium tracking-tight font-bold text-2xl">Výrobní kód</h3>
+        <br />
         <a class="text-white mt-2 text-3xl font-bold">
-            <?php echo $second ?>
+            <?php echo $third ?>
         </a>
-        <a class="ml-5 text-white mt-2 text-3xl font-bold">
-            Testovací firma
+    </div>
+    <div class="mx-auto w-10/12 md:w-1/2 lg:w-1/4 bg-blue-600 rounded-lg px-6 py-8 m-3 ring-1 ring-slate-900/5 shadow-xl" id="dropdown">
+        <div>
+            <span class="inline-flex items-center justify-center p-2 bg-slate-300 rounded-md shadow-lg">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-calendar2-range" viewBox="0 0 16 16">
+                  <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z"/>
+                  <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4zM9 8a1 1 0 0 1 1-1h5v2h-5a1 1 0 0 1-1-1zm-8 2h4a1 1 0 1 1 0 2H1v-2z"/>
+                </svg>
+            </span>
+        </div>
+        <h3 class="text-black mt-5 text-base font-medium tracking-tight font-bold text-2xl">Týden výroby</h3>
+        <br />
+        <a class="text-white mt-2 text-3xl font-bold">
+            <?php echo $fourth ?>. týden
+        </a>
+    </div>
+    <div class="mx-auto w-10/12 md:w-1/2 lg:w-1/4 bg-purple-700 rounded-lg px-6 py-8 m-3 ring-1 ring-slate-900/5 shadow-xl" id="dropdown">
+        <div>
+            <span class="inline-flex items-center justify-center p-2 bg-slate-300 rounded-md shadow-lg">
+               <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-calendar3" viewBox="0 0 16 16">
+                  <path d="M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857V3.857z"/>
+                  <path d="M6.5 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+                </svg>
+            </span>
+        </div>
+        <h3 class="text-black mt-5 text-base font-medium tracking-tight font-bold text-2xl">Rok výroby</h3>
+        <br />
+        <a class="text-white mt-2 text-3xl font-bold">
+            20<?php echo $fifth ?>
         </a>
     </div>
 </div>
